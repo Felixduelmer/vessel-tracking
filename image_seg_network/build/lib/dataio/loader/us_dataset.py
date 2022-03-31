4cf0a3a8531479991322a42f7eefbd80023272df
Felixduelmer <felix-duelmer@online.de>
2022-03-31 08:25:55 +0200

    feat: enable dev

---
+++ image_seg_network/build/lib/dataio/loader/us_dataset.py
@@ -0,0 +1,101 @@
+import cv2
+import torch
+import torch.utils.data as data
+import h5py
+import numpy as np
+import datetime
+
+from os import listdir
+from os.path import join
+from .utils import check_exceptions
+
+
+class UltraSoundDataset(data.Dataset):
+    def __init__(self, root_path, seq_len, split, transform=None, preload_data=False):
+        super(UltraSoundDataset, self).__init__()
+
+        f = h5py.File(root_path, 'r')
+
+        self.images = f['x_' + split]
+
+        self.seq_len = seq_len
+
+        if preload_data:
+            self.images = np.array(self.images[:])
+
+        self.labels = np.expand_dims(
+            np.array(f['y_' + split][:], dtype=np.uint8), axis=1)  # [:1000]
+
+        assert len(self.images) == len(self.labels)
+
+        # reduce image to gray scale
+        grayimages = []
+        for idx in range(len(self.images)):
+            grayimages.append(np.array([cv2.cvtColor(
+                self.images[idx, j, :, :, :], cv2.COLOR_BGR2GRAY) for j in range(2)]))
+        self.images = np.array(grayimages)
+        # prepare sequences
+        if len(self.images) % self.seq_len != 0:
+            if split == 'train':
+                self.images = np.concatenate(
+                    (self.images, np.zeros((self.seq_len - (len(self.images) % self.seq_len), *self.images.shape[1:]))))
+                self.labels = np.concatenate(
+                    (self.labels, np.zeros((self.seq_len - (len(self.labels) % self.seq_len), *self.labels.shape[1:]))))
+            else:
+                self.images = self.images[:(
+                        len(self.images) - (len(self.images) % self.seq_len))]
+                self.labels = self.labels[:(
+                        len(self.labels) - (len(self.labels) % self.seq_len))]
+
+        self.images = self.images.reshape(int(np.ceil(len(self.images) / self.seq_len)),
+                                          self.seq_len, *self.images.shape[1:])
+        self.labels = self.labels.reshape(int(np.ceil(len(self.labels) / self.seq_len)),
+                                          self.seq_len, *self.labels.shape[1:])
+
+        # print(class_weight)
+        assert len(self.images) == len(self.labels)
+
+        # data augmentation
+        self.transform = transform
+
+        # report the number of images in the dataset
+        print('Number of images: {0}'.format(self.__len__()))
+
+    def __getitem__(self, index):
+        # update the seed to avoid workers sample the same augmentation parameters
+        np.random.seed(datetime.datetime.now().second +
+                       datetime.datetime.now().microsecond)
+
+        # load the images
+        input = self.images[index]
+        target = self.labels[index]
+
+        # handle exceptions
+        # check_exceptions(input, target)
+        # random number generator seed is saved to have some transformation for all images in one sequence
+        # https://github.com/pytorch/vision/issues/9
+        if self.transform:
+            state = torch.get_rng_state()
+            for i in range(len(input)):
+                torch.set_rng_state(state)
+                input[i][0] = self.transform(torch.from_numpy(input[i, [0]]))
+                torch.set_rng_state(state)
+                input[i][1] = self.transform(torch.from_numpy(input[i, [1]]))
+                torch.set_rng_state(state)
+                target[i] = self.transform(torch.from_numpy(target[i]))
+
+        # print(input.shape, torch.from_numpy(np.array([target])))
+        # print("target", np.int64(target))
+        return input, target
+
+    def __len__(self):
+        return len(self.images)
+
+
+if __name__ == '__main__':
+    dataset = UltraSoundDataset(
+        '/home/felix/projects/ma/data/h5_datasets/ultrasound.h5')
+
+    from torch.utils.data import DataLoader, sampler
+
+    ds = DataLoader(dataset=dataset, num_workers=1, batch_size=2)
---
+++ image_seg_network/build/lib/models/feedforward_seg_model.py
@@ -0,0 +1,175 @@
+import torch
+from torch.autograd import Variable
+import torch.optim as optim
+
+from collections import OrderedDict
+import utils.util as util
+from .base_model import BaseModel
+from .networks import get_network
+from .layers.loss import *
+from .networks_other import get_scheduler, print_network, benchmark_fp_bp_time
+from .utils import segmentation_stats, get_optimizer, get_criterion
+from .networks.utils import HookBasedFeatureExtractor
+
+
+class FeedForwardSegmentation(BaseModel):
+
+    def name(self):
+        return 'FeedForwardSegmentation'
+
+    def initialize(self, opts, **kwargs):
+        BaseModel.initialize(self, opts, **kwargs)
+        self.isTrain = opts.isTrain
+        self.has_hidden = opts.has_hidden
+
+        # define network input and output pars
+        self.input = None
+        self.target = None
+        self.hidden = None
+
+        # load/define networks
+        self.net = get_network(opts.model_type, in_channels=opts.input_nc, nonlocal_mode=opts.nonlocal_mode, feature_scale=opts.feature_scale,
+                               attention_dsample=opts.attention_dsample, bptt_step=opts.bptt_step)
+        if self.use_cuda:
+            self.net = self.net.cuda()
+
+        # load the model if a path is specified or it is in inference mode
+        if not self.isTrain or opts.continue_train:
+            self.path_pre_trained_model = opts.path_pre_trained_model
+            if self.path_pre_trained_model:
+                self.load_network_from_path(
+                    self.net, self.path_pre_trained_model, strict=False)
+                self.which_epoch = int(0)
+            else:
+                self.which_epoch = opts.which_epoch
+                self.load_network(self.net, 'S', self.which_epoch)
+
+        # training objective
+        if self.isTrain:
+            self.criterion = get_criterion(opts)
+            # initialize optimizers
+            self.schedulers = []
+            self.optimizers = []
+            self.optimizer_S = get_optimizer(opts, self.net.parameters())
+            self.optimizers.append(self.optimizer_S)
+
+            # print the network details
+            # print the network details
+            if kwargs.get('verbose', True):
+                print('Network is initialized')
+                print_network(self.net)
+
+    def set_scheduler(self, train_opt):
+        for optimizer in self.optimizers:
+            self.schedulers.append(get_scheduler(optimizer, train_opt))
+            print('Scheduler is added for optimiser {0}'.format(optimizer))
+
+    def init_hidden(self):
+        self.hidden = None
+
+    def set_input(self, *inputs):
+        # self.input.resize_(inputs[0].size()).copy_(inputs[0])
+        for idx, _input in enumerate(inputs):
+            # Define that it's a cuda array
+            if idx == 0:
+                self.input = _input.cuda().float() if self.use_cuda else _input.float()
+            elif idx == 1:
+                self.target = Variable(
+                    _input.cuda()).float() if self.use_cuda else Variable(_input).float()
+                assert len(self.input) == len(self.target)
+
+    def forward(self, split):
+        if split == 'train':
+            if self.has_hidden:
+                self.prediction, self.hidden = self.net(
+                    Variable(self.input), self.hidden)
+            else:
+                self.prediction = self.net(Variable(self.input))
+
+        elif split == 'test':
+            if self.has_hidden:
+                self.prediction, _ = self.net(
+                    Variable(self.input, requires_grad=False), self.hidden)
+            else:
+                self.prediction = self.net(
+                    Variable(self.input, requires_grad=False))
+            self.prediction = self.net.apply_sigmoid(self.prediction)
+            # Apply a softmax and return a segmentation map
+            self.pred_seg = torch.round(self.prediction.data)*255
+
+    def backward(self):
+        self.loss_S = self.criterion(self.prediction, self.target)
+        self.loss_S.backward()
+        if self.has_hidden:
+            self.hidden = [h.detach() for h in self.hidden]
+
+    def optimize_parameters(self):
+        self.net.train()
+        self.forward(split='train')
+
+        self.optimizer_S.zero_grad()
+        self.backward()
+        self.optimizer_S.step()
+
+    # This function updates the network parameters every "accumulate_iters"
+    def optimize_parameters_accumulate_grd(self, iteration):
+        accumulate_iters = int(2)
+        if iteration == 0:
+            self.optimizer_S.zero_grad()
+        self.net.train()
+        self.forward(split='train')
+        self.backward()
+
+        if iteration % accumulate_iters == 0:
+            self.optimizer_S.step()
+            self.optimizer_S.zero_grad()
+
+    def test(self):
+        self.net.eval()
+        self.forward(split='test')
+
+    def validate(self):
+        self.net.eval()
+        self.forward(split='test')
+        self.loss_S = self.criterion(self.prediction, self.target)
+
+    def get_segmentation_stats(self):
+        self.seg_scores, self.dice_score = segmentation_stats(
+            self.prediction, self.target)
+        seg_stats = [('Overall_Acc', self.seg_scores['overall_acc']),
+                     ('Mean_IOU', self.seg_scores['mean_iou'])]
+        for class_id in range(self.dice_score.size):
+            seg_stats.append(('Class_{}'.format(class_id),
+                             self.dice_score[class_id]))
+        return OrderedDict(seg_stats)
+
+    def get_current_errors(self):
+        return OrderedDict([('Seg_Loss', self.loss_S.item())
+                            ])
+
+    def get_current_visuals(self, labels):
+        inp_img = util.tensor2im(self.input[:, [0], :, :], 'bmode')
+        inp_doppler = util.tensor2im(self.input[:, [1], :, :], 'doppler')
+        seg_img = util.tensor2im(self.pred_seg, 'lbl')
+        ground_truth = util.tensor2im(labels, 'ground_truth')
+        return OrderedDict([('out_S', seg_img), ('ground_truth', ground_truth), ('inp_S', inp_img), ('inp_doppler', inp_doppler)])
+
+    def get_feature_maps(self, layer_name, upscale):
+        feature_extractor = HookBasedFeatureExtractor(
+            self.net, layer_name, upscale)
+        return feature_extractor.forward(Variable(self.input))
+
+    # returns the fp/bp times of the model
+    def get_fp_bp_time(self, size=None):
+        if size is None:
+            size = (1, 1, 160, 160, 96)
+
+        inp_array = Variable(torch.zeros(*size)).cuda()
+        out_array = Variable(torch.zeros(*size)).cuda()
+        fp, bp = benchmark_fp_bp_time(self.net, inp_array, out_array)
+
+        bsize = size[0]
+        return fp/float(bsize), bp/float(bsize)
+
+    def save(self, epoch_label):
+        self.save_network(self.net, 'S', epoch_label, self.gpu_ids)
